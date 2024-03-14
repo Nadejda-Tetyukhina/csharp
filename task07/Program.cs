463ead1f03707c29123c4aba0f9ccb9326eed2f4
Nadezhda Tetyukhina <Lalimnik@gmail.com>
2024-03-14 18:21:14 +0300

    задания семинара 1

---
+++ task07/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Напишите программу, которая принимает на вход трехзначное число и на выходе показывает последнюю цифру этого числа*/
+
+Console.WriteLine("Введи трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 99 && number < 1000)
+{
+    Console.WriteLine(number%10);
+}
+else
+{
+    Console.WriteLine("число не трехзначное");
+}
