74a577000cdd99c87ea161f182bc1ab8d231181f
Nadezhda Tetyukhina <Lalimnik@gmail.com>
2024-03-17 20:06:38 +0300

    с2з6

---
+++ tasknum/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа
+и проверяет является ли одно квадратом другого
+5, 25 да
+-4, 16 да
+25, 5 да
+8, 9 нет
+*/
+Console.WriteLine("введите два числа ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 * num1 == num2 | num2 * num2 == num1)
+{
+    System.Console.WriteLine("Да");
+}
+else { System.Console.WriteLine("Нет"); }
